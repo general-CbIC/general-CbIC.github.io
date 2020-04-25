5fc885eb8b84da2415bae10c9ca851f042f30ca3
Aleksandr Sysoev <sanchous.ok@gmail.com>
2020-04-25 15:58:17 +0700

    Remake via react

---
+++ precache-manifest.d81fa184ccd79ec8ba515adba12f1c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d8ff695efbea6a2b44ad511e4ee93d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52ea4064f3ff55c7c14f",
+    "url": "/static/css/main.ed96fa2b.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ea4064f3ff55c7c14f",
+    "url": "/static/js/main.85450e65.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
